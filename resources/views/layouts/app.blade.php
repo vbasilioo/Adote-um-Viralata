55b2187e9a6c12a58fab4847a22fce313b243683
vbasilioo <vbasilio2019@gmail.com>
2024-06-20 20:35:59 -0300

    <FEATURE>[Home / Login]: adicionado layout base, layout de login e layout home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adote um Viralata</title>
+    <title>@yield('title', 'Adote um Viralata')</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        @yield('content')
+    </div>
+    @vite('resources/js/app.js')
 </body>
 </html>
--- routes/web/index.php
+++ routes/web/index.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home/home');
 });
+
+Route::get('/login', function () {
+    return view('login/index');
+})->name('login');
