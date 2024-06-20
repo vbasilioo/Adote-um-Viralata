96bb209162348099f59ef6e5cc95a95148941c0c
vbasilioo <vbasilio2019@gmail.com>
2024-06-20 19:47:44 -0300

    <CHORE>[Rotas]: ajustando rotas, acrescentando api e web index

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api/index.php',
+        web: __DIR__.'/../routes/web/index.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
